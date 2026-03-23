6ac3cea72b364ebfe0f9edb96a2947e9a234898a
César Filipe <filipegomes012642@gmail.com>
2026-03-23 20:51:06 -0300

    adicionando dependencias do projeto0

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tabalho/main.dart';
+import 'package:eco_share/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
